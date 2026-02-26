5859ab2cf2359f7d0e78ee2ca598f200101e9561
Ferdinando <ferdinando.primerano@gmail.com>
2026-02-26 10:21:48 +0100

    First commit

---
+++ src/app/todays-arrivals/todays-arrivals.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todays-arrivals',
+  imports: [],
+  templateUrl: './todays-arrivals.html',
+  styleUrl: './todays-arrivals.css',
+})
+export class TodaysArrivals {
+
+}
