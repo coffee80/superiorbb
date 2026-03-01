ea1829ea9ace99f9e69494fdf62b272739427f5a
Ferdinando <ferdinando.primerano@gmail.com>
2026-03-01 21:08:00 +0100

    Aggiunta una guard su tutto ciò che è non loggato

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { BookRoom } from './book-room/book-room';
 import { FormLogin } from './form-login/form-login';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-    { path:'', component:BookRoom},
+    { path:'', component:BookRoom, canActivate: [authGuard]},
     { path:'login', component:FormLogin}
 ];
